06fcd8bdeeeca959c566e1ef41779ef835f9f1f4
knomaeb <sunil.rana320778@gmail.com>
2025-03-26 09:36:52 +0530

    setup splashscreen

--- app/src/main/java/com/example/khabar/MainActivity.kt
+++ app/src/main/java/com/example/khabar/MainActivity.kt
@@ -4,23 +4,46 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.khabar.ui.theme.KhabarTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         enableEdgeToEdge()
         setContent {
             KhabarTheme {
-
+                Greeting()
             }
+        }
+    }
+}
+
+@Composable
+fun Greeting(){
+    Scaffold(
+        modifier = Modifier.fillMaxSize()
+    ){
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(it)
+        ){
+            Text(
+                text = "Khabar",
+                fontWeight = FontWeight.Bold
+            )
         }
     }
 }
