7aadd77a1adae86b00b2a5f30a38204c35067626
Sunil Rana <sunil.rana320778@gmail.com>
2025-05-22 20:34:00 +0530

    update dagger hilt and dependencies

--- app/src/main/java/com/example/khabar/di/module/ApplicationModule.kt
+++ app/src/main/java/com/example/khabar/di/module/ApplicationModule.kt
@@ -1,27 +1,35 @@
 package com.example.khabar.di.module
 
 import android.content.Context
-import androidx.room.Room
 import androidx.browser.customtabs.CustomTabsIntent
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.room.Room
+import com.example.khabar.core.dispatcher.DefaultDispatcherProvider
+import com.example.khabar.core.dispatcher.DispatcherProvider
+import com.example.khabar.core.logger.DefaultLogger
+import com.example.khabar.core.logger.Logger
+import com.example.khabar.core.network.NetworkConnected
+import com.example.khabar.core.network.NetworkHelper
+import com.example.khabar.core.utils.AppConstants
+import com.example.khabar.core.utils.AppConstants.API_KEY
 import com.example.khabar.data.local.AppDatabase
 import com.example.khabar.data.local.AppDatabaseService
 import com.example.khabar.data.local.DatabaseService
+import com.example.khabar.data.local.entity.Article
+import com.example.khabar.data.paging.NewsPagingSource
 import com.example.khabar.data.remote.AuthInterceptor
 import com.example.khabar.data.remote.NetworkService
-import com.example.khabar.utils.AppConstants.API_KEY
-import com.example.khabar.utils.DefaultDispatcherProvider
-import com.example.khabar.utils.DispatcherProvider
-import com.example.khabar.utils.network.NetworkConnected
+import com.example.khabar.di.ApiKey
+import com.example.khabar.di.BaseUrl
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
-import kotlinx.serialization.json.Json
-import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
-import retrofit2.converter.kotlinx.serialization.asConverterFactory
+import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
 
 
@@ -31,33 +39,37 @@ class ApplicationModule {
 
     @Singleton
     @Provides
-    fun provideNetworkConnected(@ApplicationContext context: Context): NetworkConnected{
+    fun provideNetworkConnected(@ApplicationContext context: Context): NetworkConnected {
         return NetworkConnected(context)
     }
 
     @Singleton
     @Provides
-    fun provideCustomTabIntent() : CustomTabsIntent{
+    fun provideGsonConverterFactory(): GsonConverterFactory = GsonConverterFactory.create()
+
+    @Singleton
+    @Provides
+    fun provideCustomTabIntent(): CustomTabsIntent {
         return CustomTabsIntent.Builder().build()
     }
 
     @BaseUrl
     @Singleton
     @Provides
-    fun provideBaseUrl(): String {
-        return "https://newsapi.org/v2/"
-    }
+    fun provideBaseUrl(): String = "https://newsapi.org/v2/"
 
     @ApiKey
     @Singleton
     @Provides
-    fun provideApiKey(): String {
-        return API_KEY
-    }
+    fun provideApiKey(): String = API_KEY
 
     @Singleton
     @Provides
-    fun provideNetworkService(@BaseUrl baseUrl: String, @ApiKey apiKey: String): NetworkService {
+    fun provideNetworkService(
+        @BaseUrl baseUrl: String,
+        @ApiKey apiKey: String,
+        gsonFactory: GsonConverterFactory
+    ): NetworkService {
 
         val authInterceptor = AuthInterceptor(apiKey = apiKey)
 
@@ -65,24 +77,47 @@ class ApplicationModule {
             .addInterceptor(authInterceptor)
             .build()
 
-        val contentType = "application/json".toMediaType()
-
-        val json = Json {
-            ignoreUnknownKeys = true
-            coerceInputValues = true
-        }
-
         return Retrofit.Builder()
             .baseUrl(baseUrl)
             .client(client)
-            .addConverterFactory(json.asConverterFactory(contentType))
+            .addConverterFactory(gsonFactory)
             .build()
             .create(NetworkService::class.java)
     }
 
     @Provides
     @Singleton
-    fun provideAppDatabase(@ApplicationContext context: Context): AppDatabase {
+    fun provideLogger(): Logger = DefaultLogger()
+
+    @Provides
+    @Singleton
+    fun provideDispatcher(): DispatcherProvider = DefaultDispatcherProvider()
+
+    @Provides
+    @Singleton
+    fun providePager(
+        newsPagingSource: NewsPagingSource
+    ): Pager<Int, Article> {
+        return Pager(
+            config = PagingConfig(
+                AppConstants.DEFAULT_QUERY_PAGE_SIZE
+            )
+        ) {
+            newsPagingSource
+        }
+    }
+
+    @Provides
+    @Singleton
+    fun provideNetworkHelper(
+        @ApplicationContext context: Context
+    ): NetworkHelper {
+        return NetworkConnected(context)
+    }
+
+    @Provides
+    @Singleton
+    fun provideArticleDatabase(@ApplicationContext context: Context): AppDatabase {
         return Room.databaseBuilder(
             context = context,
             klass = AppDatabase::class.java,
@@ -95,8 +130,4 @@ class ApplicationModule {
     fun provideDatabaseService(appDatabase: AppDatabase): DatabaseService {
         return AppDatabaseService(appDatabase)
     }
-
-    @Provides
-    @Singleton
-    fun provideDispatcherProvider(): DispatcherProvider = DefaultDispatcherProvider()
 }
