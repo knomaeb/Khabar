c16dc77844eccd3855f7945d8c40bf5bdf04d245
Sunil Rana <sunil.rana320778@gmail.com>
2025-08-04 09:59:34 +0530

    Refactor NewsPagingSource to fetch news from API

--- app/src/main/java/com/example/khabar/data/paging/NewsPagingSource.kt
+++ app/src/main/java/com/example/khabar/data/paging/NewsPagingSource.kt
@@ -2,53 +2,42 @@ package com.example.khabar.data.paging
 
 import androidx.paging.PagingSource
 import androidx.paging.PagingState
-import com.example.khabar.core.dispatcher.DispatcherProvider
-import com.example.khabar.core.network.NetworkConnected
-import com.example.khabar.core.network.NoInternetException
-import com.example.khabar.core.utils.AppConstants
-import com.example.khabar.data.local.entity.Article
-import com.example.khabar.data.repository.MainRepository
-import kotlinx.coroutines.withContext
+import com.example.khabar.data.remote.NetworkService
+import com.example.khabar.data.remote.dto.NewsResponseDto.Companion.toNews
+import com.example.khabar.domain.model.News
 import javax.inject.Inject
 
 class NewsPagingSource @Inject constructor(
-    private val newsRepository: MainRepository,
-    private val networkConnected: NetworkConnected,
-    private val dispatcherProvider: DispatcherProvider
-) : PagingSource<Int, Article>() {
-    override fun getRefreshKey(state: PagingState<Int, Article>): Int? {
-        return state.anchorPosition
+    private val newsApi: NetworkService,
+    private val country: String?,
+    private val category: String?,
+    private val searchQuery: String?
+) : PagingSource<Int, News>() {
+    override fun getRefreshKey(state: PagingState<Int, News>): Int? = state.anchorPosition
+
+    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, News> {
+        return try {
+            val nextPage = params.key ?: FIRST_PAGE_INDEX
+            val response =
+                newsApi.fetchNews(
+                    country = country,
+                    category = category,
+                    searchQuery = searchQuery,
+                    pageSize = params.loadSize,
+                    page = nextPage
+                )
+
+            LoadResult.Page(
+                data = response.articles.map { it.toNews() },
+                prevKey = if (nextPage == FIRST_PAGE_INDEX) null else nextPage - 1,
+                nextKey = if (response.articles.isEmpty()) null else nextPage + 1
+            )
+        } catch (exception: Exception) {
+            return LoadResult.Error(exception)
+        }
     }
 
-    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, Article> {
-        val page = params.key ?: 1
-        lateinit var loadResult: LoadResult<Int, Article>
-
-        withContext(dispatcherProvider.io) {
-            kotlin.runCatching {
-                if (!networkConnected.inNetworkConnected()) {
-                    if (page == AppConstants.DEFAULT_PAGE_NUM) {
-                        val articles = newsRepository.getNewsFromDb()
-                        loadResult = LoadResult.Page(
-                            data = articles,
-                            prevKey = page.minus(1),
-                            nextKey = if (articles.isEmpty()) null else page.plus(1)
-                        )
-                    } else {
-                        throw NoInternetException()
-                    }
-                } else {
-                    val articles = newsRepository.getNews(page)
-                    loadResult = LoadResult.Page(
-                        data = articles,
-                        prevKey = if (page == 1) null else page.minus(1),
-                        nextKey = if (articles.isEmpty()) null else page.plus(1)
-                    )
-                }
-            }.onFailure {
-                loadResult = LoadResult.Error(it)
-            }
-        }
-        return loadResult
+    companion object {
+        const val FIRST_PAGE_INDEX = 0
     }
 }
