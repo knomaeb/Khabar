b78fa1632c33904e89abe84b0bae85dd46def6d1
Sunil Rana <sunil.rana320778@gmail.com>
2025-08-04 10:00:06 +0530

    Add NewsResponseDto and update NetworkService to fetch news

--- app/src/main/java/com/example/khabar/data/remote/NetworkService.kt
+++ app/src/main/java/com/example/khabar/data/remote/NetworkService.kt
@@ -1,12 +1,11 @@
 package com.example.khabar.data.remote
 
-import com.example.khabar.core.utils.AppConstants.DEFAULT_COUNTRY
-import com.example.khabar.core.utils.AppConstants.DEFAULT_LANGUAGE
+import com.example.khabar.BuildConfig
+import com.example.khabar.core.utils.AppConstants.DEFAULT_CATEGORY_CODE
+import com.example.khabar.core.utils.AppConstants.DEFAULT_COUNTRY_CODE
 import com.example.khabar.core.utils.AppConstants.DEFAULT_PAGE_NUM
 import com.example.khabar.core.utils.AppConstants.DEFAULT_QUERY_PAGE_SIZE
-import com.example.khabar.core.utils.AppConstants.DEFAULT_SOURCE
-import com.example.khabar.data.model.News
-import com.example.khabar.data.model.Sources
+import com.example.khabar.data.remote.dto.NewsResponseDto
 import retrofit2.http.GET
 import retrofit2.http.Query
 import javax.inject.Singleton
@@ -15,41 +14,12 @@ import javax.inject.Singleton
 interface NetworkService {
 
     @GET("top-headlines")
-    suspend fun getNews(
-        @Query("country") countryCode: String = DEFAULT_COUNTRY,
-        @Query("page") pageNum: Int = DEFAULT_PAGE_NUM,
+    suspend fun fetchNews(
+        @Query("country") country: String? = DEFAULT_COUNTRY_CODE,
+        @Query("category") category: String? = DEFAULT_CATEGORY_CODE,
+        @Query("q") searchQuery: String?,
+        @Query("page") page: Int = DEFAULT_PAGE_NUM,
         @Query("pageSize") pageSize: Int = DEFAULT_QUERY_PAGE_SIZE,
-    ): News
-
-    @GET("top-headlines")
-    suspend fun getNewsByLang(
-        @Query("language") language: String = DEFAULT_LANGUAGE,
-        @Query("page") pageNum: Int = DEFAULT_PAGE_NUM,
-        @Query("pageSize") pageSize: Int = DEFAULT_QUERY_PAGE_SIZE,
-    ): News
-
-    @GET("top-headlines")
-    suspend fun getNewsBySource(
-        @Query("sources") sources: String = DEFAULT_SOURCE,
-        @Query("page") pageNum: Int = DEFAULT_PAGE_NUM,
-        @Query("pageSize") pageSize: Int = DEFAULT_QUERY_PAGE_SIZE,
-    ): News
-
-    @GET("top-headlines")
-    suspend fun getNewsByCategory(
-        @Query("category") category: String,
-        @Query("page") pageNum: Int = DEFAULT_PAGE_NUM,
-        @Query("pageSize") pageSize: Int = DEFAULT_QUERY_PAGE_SIZE,
-    ): News
-
-
-    @GET("everything")
-    suspend fun searchNews(
-        @Query("q") searchQuery: String,
-        @Query("page") pageNum: Int = DEFAULT_PAGE_NUM,
-        @Query("pageSize") pageSize: Int = DEFAULT_QUERY_PAGE_SIZE,
-    ): News
-
-    @GET("sources")
-    suspend fun getSources(): Sources
+        @Query("apiKey") apiKey: String = BuildConfig.API_KEY,
+    ): NewsResponseDto
 }
